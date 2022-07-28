5e3bee4a7d9236373cc862d7de72e35e516769aa
mirawdy <remuparadox@gmail.com>
2022-07-28 10:39:08 +0800

    moving prev work to react components

---
+++ src/components/SectionHero/index.jsx
@@ -0,0 +1,21 @@
+import './SectionHero.css'
+import HeroCar from '../../assets/img_car.png'
+
+function Hero(){
+    return(
+        <>
+       <div className="banner">
+        <div className="banner-text">
+            <h1>Sewa & Rental Mobil Terbaik di Kawasan (Lokasimu)</h1>
+            <p>Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</p>
+            <button>Mulai Sewa Mobil</button>
+        </div>
+        <div className="banner-img">
+            <img src={HeroCar} alt="An image of black car"/>
+        </div>
+       </div>
+    </>
+    )
+}
+
+export default Hero;
---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import Hero from '../components/SectionHero'
+
+function Home(){
+    return(
+        <>
+        <Hero/>
+        </>
+    )
+}
+
+export default Home
